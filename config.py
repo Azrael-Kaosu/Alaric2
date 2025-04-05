423d4286841c2f6b3c70fbefa383d254a6462b72
Azrael-Kaosu <danielgames20333@gmail.com>
2025-04-05 00:45:34 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_KEY = "MsyHcLLcfEVaYxSYHKz4sRep4cB7dLtkkySi0fn1wgXvde0F788RkWdjrXleZmIY"
+API_SECRET = "LnzMvpPBSqWTt1gvni0pnISGkFzUVsDWcPcWXzZI6BsEr3FKnzQpNWFWXy27lvhk"
+VALOR_BR = 60
+STOP_LOSS = 0.02  # ainda não usado
+TAKE_PROFIT = 0.003  # ainda não usado
