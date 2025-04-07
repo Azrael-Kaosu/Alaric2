d4a31fd8f6a05d34b99f00bff83ce432aed177d3
Azrael-Kaosu <danielgames20333@gmail.com>
2025-04-07 19:11:13 -0300

    Create web.py

---
+++ web.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import threading
+from bot import bot_loop  # importamos o loop do bot
+
+app = Flask(__name__)
+
+@app.route('/')
+def health():
+    return 'OK', 200
+
+# Iniciar o bot em thread separada
+threading.Thread(target=bot_loop, daemon=True).start()
+
+# Iniciar o servidor Flask
+if __name__ == '__main__':
+    app.run
